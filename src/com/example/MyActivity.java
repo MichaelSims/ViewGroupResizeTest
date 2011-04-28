4004389221306561e2999c4b435abbbeea5cdd87
Michael Sims <msims@mercuryintermedia.com>
2011-04-28 17:22:00 -0500

    Add a frame layout that I'm going to resize

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -2,14 +2,25 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
-public class MyActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MyActivity extends Activity {
+
+    private FrameLayout resizeableFrame;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        resizeableFrame = (FrameLayout) findViewById(R.id.resizableFrame);
+
+        ViewGroup.LayoutParams params = resizeableFrame.getLayoutParams();
+        params.width = 300;
+        params.height = 300;
     }
+
 }
