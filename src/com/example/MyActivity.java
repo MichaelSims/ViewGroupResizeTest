31d99e567d1ae68ceba29338b9d4543f22486f5c
Michael Sims <msims@mercuryintermedia.com>
2011-04-29 15:08:55 -0500

    Add another RelativeLayout to match the layout of the view that I'm modeling this after

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -1,26 +1,38 @@
 package com.example;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class MyActivity extends Activity {
 
     private FrameLayout resizeableFrame;
+    private RelativeLayout viewContainer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        resizeableFrame = (FrameLayout) findViewById(R.id.resizableFrame);
+        initializeViews();
 
+        /* Set a static size of the resizeable frame */
         ViewGroup.LayoutParams params = resizeableFrame.getLayoutParams();
         params.width = 300;
         params.height = 300;
+
+    }
+
+    private void initializeViews() {
+        resizeableFrame = (FrameLayout) findViewById(R.id.resizableFrame);
+        viewContainer = (RelativeLayout) findViewById(R.id.manuallySizedViewContainer);
     }
 
 }
